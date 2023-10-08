885e81ea7c6b02ffbaee73494228b58852b52e70
PeDrumond <pedrosdrumond@gmail.com>
2023-10-08 19:12:07 -0300

    Feitas alterações

--- Bootcamp DIO/Estacionamento/Pessoa.cs
+++ Bootcamp DIO/Estacionamento/Pessoa.cs
@@ -10,10 +10,11 @@ namespace Estacionamento
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string RepresentanteLegal { get; set; }
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos. Sou: {RepresentanteLegal}");
         }
     }
 }
--- Bootcamp DIO/Estacionamento/Program.cs
+++ Bootcamp DIO/Estacionamento/Program.cs
@@ -3,5 +3,6 @@
 Pessoa pessoa = new Pessoa();
 pessoa.Nome = "Pedro Drumond";
 pessoa.Idade = 29;
+pessoa.RepresentanteLegal = "Maior e capaz";
 
 pessoa.Apresentar();
