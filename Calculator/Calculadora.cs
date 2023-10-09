27c179befbf77b84a3644fd1080d4dd2452c97a0
Pedro Silva Drumond <69291526+PeDrumond@users.noreply.github.com>
2023-10-09 12:25:43 -0300

    Create Calculadora.cs

---
+++ Calculator/Calculadora.cs
@@ -0,0 +1,65 @@
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}º = {Math.Round(seno, 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo}º = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tan = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}º = {Math.Round(tan, 4)}");
+        }
+    }
+}
+
+// Calculadora calculadora = new Calculadora();
+// calculadora.Somar(10, 30)
+// calculadora.Subtrair(10, 30)
+// calculadora.Multiplicar(10, 30)
+// calculadora.Dividir(10, 30)
